85417fcfebe7e53ada53f51be8a3dbc83853accf
Andrey Chernikov <linuxoid.andrew@gmail.com>
2023-12-21 19:27:38 +0300

    Typescript

--- frontend/src/views/Anticipation.vue
+++ frontend/src/views/Anticipation.vue
@@ -11,60 +11,89 @@
                   :placeholder="$t('anticipation.placeHolderSelect')"
                   @update:value="removeVowels"
         />
-      </n-form-item></div>
+      </n-form-item>
+        <n-form-item :label="$t('anticipation.Mode')">
+          <n-select v-model:value="cipherMode"
+                    :options="cipherModeOptions"
+                    :placeholder="$t('anticipation.placeHolderSelect')"
+                    @update:value="removeVowels"
+          />
+</n-form-item>
+      </div>
     </n-form>
   </n-space>
+<BaseTextBox v-if="parsedText.length > 0">
+  {{parsedText}}
+</BaseTextBox>
 
       <BaseTextBox ref="textContent" v-if="parsedText.length > 0">
-      <span v-for="item in parsedText" :class="getCharClass(item)">{{item.char}}</span>
+      <div v-if="cipherMode === 'dropLetters'">
+        <span v-for="item in parsedText" :class="getCharClass(item)">{{item.char}}</span>
+      </div>
+        <div v-else>
+          {{parsedText.map(item=> item.char).join('')}}
+        </div>
         <div class="py-4">
           <BaseButton :label="$t('common.copyText')"  @button-clicked="copyText()"/></div>
       </BaseTextBox>
 </template>
 
-<script>
-import BaseInput from "@/components/BaseInput.vue";
-import BaseButton from "@/components/BaseButton.vue";
-import {NForm, NFormItem, NSelect, NSpace} from "naive-ui";
-import TextParser from "@/helpers/text-parser";
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+import BaseInput from "@/components/BaseInput";
+import BaseButton from "@/components/BaseButton";
+import { NForm, NFormItem, NSelect, NSpace } from "naive-ui";
+import TextParser from "@/helpers/parser";
 import BaseTextBox from "@/components/BaseTextBox.vue";
 import charSets from "@/helpers/charSets";
-export default {
+export default defineComponent({
   name: "Anticipation",
-  components: {BaseTextBox, BaseButton, BaseInput, NSpace, NForm, NSelect, NFormItem},
-  data(){
-    return {
-      sourceText: '',
-      parsedText: [],
-      additionalChars: [],
-      russianConsonants: charSets.russianAlphabet.selectConsonants
-    }
-  },
-  methods: {
-      copyText() {
+  components: { BaseTextBox, BaseButton, BaseInput, NSpace, NForm, NSelect, NFormItem },
+  setup() {
+    const sourceText = ref<string>('');
+    const cipherMode = ref<string>('dropLetters');
+    const parsedText = ref<any[]>([]);
+    const additionalChars = ref<string[]>([]);
+    const russianConsonants = charSets.russianAlphabet.selectConsonants;
+
+
+  const copyText = () => {
           let textToCopy = this.$refs.textContent;
           let blob = textToCopy.$el;
           const range = document.createRange();
           range.selectNode(blob);
-          window.getSelection().removeAllRanges()
+          window.?getSelection().?removeAllRanges()
           const selection = window.getSelection();
           selection.addRange(range);
           document.execCommand("copy");
           window.getSelection().removeAllRanges()
-      },
-    textUpdated(value){
-      this.sourceText = value
-      this.removeVowels()
-    },
+      }
+    const textUpdated = (value: string) => {
+      sourceText.value = value;
+      const parser = new TextParser(sourceText.value);
+      switch (cipherMode.value) {
+        case 'dropLetters':
+          parsedText.value = removeVowels();
+          break;
+        case 'replaceLetters':
+          parsedText.value = parser.replaceLetters();
+          break;
+        case 'seamlessText':
+          parsedText.value = parser.seamlessText();
+          break;
+          // Add other cases as needed
+      }
+    };
     removeVowels(){
       let parser = new TextParser(this.sourceText)
       let parsedText = parser.replaceVowels()
       parsedText.forEach((item, i) => {
         if (this.additionalChars.includes(item.char.toUpperCase()) || item.is_vowel){
           parsedText[i].char = '●'
+          parsedText[i].is_vowel = true
         }
       })
-      this.parsedText = parsedText
+      return parsedText
     },
     getCharClass(item){
       if (item.is_vowel){
@@ -73,7 +102,17 @@ export default {
         }
         return 'grayed'
       }
-
+    }
+  },
+  computed: {
+    cipherModeOptions(){
+      return [
+        {label: this.$t('anticipation.dropLetters'), value: 'dropLetters'},
+        {label: this.$t('anticipation.replaceLetters'), value: 'replaceLetters'},
+        {label: this.$t('anticipation.seamlessText'), value: 'seamlessText'},
+        {label: this.$t('anticipation.reverseText'), value: 'reverseText'},
+        {label: this.$t('anticipation.substitutionText'), value: 'substitutionText'}
+      ]
     }
   }
 }
