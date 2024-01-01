facb01cc76908bc25594622e72ea37da7fd21406
Andrey Chernikov <linuxoid.andrew@gmail.com>
2024-01-01 17:38:08 +0300

    CI Pipeline

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,7 +5,6 @@ import i18n from "./i18n";
 import router from './router';
 import naive from 'naive-ui';
 import VueGtagPlugin from "vue-gtag";
-import msClarity from "./plugins/ms-clarity";
 const app = createApp(App);
 
 app.use(naive)
@@ -17,11 +16,12 @@ if (process.env.NODE_ENV !== 'development') {
     app.use(VueGtagPlugin, {
         config: {
             cookie_domain: 'text-tools.ru',
+            // cookie_domain: 'reader.dev.andyi95.com',
             id: 'G-181EZLB3NP'  // prod
             // id: 'G-K1BZEB1T2Y'  // dev
+            // id: process.env.VITE_GTAG_ID
         }
     }, router);
-    app.use(msClarity, {clarityId: 'kdfwwrinuj'});
 }
 
 app.mount('#app');
