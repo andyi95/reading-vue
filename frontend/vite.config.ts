2f3a6039073260f5da8fe11005b6e31571636ced
Andrey Chernikov <linuxoid.andrew@gmail.com>
2024-07-09 13:16:03 +0300

    READ-78 Bug report load

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -40,8 +40,7 @@ export default defineConfig(({command, mode}) => {
                     ]
                 }),
                 Components({
-                    resolvers: [NaiveUiResolver()],
-                    dirs: ['src/components', 'src/views']
+                    resolvers: [NaiveUiResolver()]
                 }),
             ],
             resolve: {
